db242b1a2dea7e1dcec3d22302eea48e35d45138
dylanjohn <dylan@mediamachine.co.za>
2022-12-20 13:21:42 +0200

    npx create-next-app

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-blog',
+  themeConfig: './theme.config.js',
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // any configs you need
+  basePath: '/nextjs-portfolio-starter',
+  images: {
+    loader: 'akamai',
+    path: '/',
+  }
+}
+
+module.exports = withNextra(nextConfig)
